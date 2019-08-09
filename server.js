070133b5ced949cb4afbfc14fcc70063ed996b4d
Zheli Zhu <brianzzl@hotmail.com>
2019-08-09 14:35:06 -0400

    updated

--- server.js
+++ server.js
@@ -67,6 +67,7 @@ app.post("/api/tables", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
     var newTable = req.body;
+    // var newRes = req.body;
   
     // Using a RegEx Pattern to remove spaces from newCharacter
     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
